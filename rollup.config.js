3032c7a636ab6acb92dbffe8128dd5d5bf49fc7c
Charles Crete <cretezy@gmail.com>
2018-01-29 02:01:00 -0500

    Bump to 3.0.1, compile on builds (fix babel)

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,5 @@
+import babel from "rollup-plugin-babel";
+
 export default {
   input: "src/index.js",
   output: [
@@ -14,5 +16,18 @@ export default {
       format: "iife",
       name: "scrypt"
     }
+  ],
+  plugins: [
+    babel({
+      presets: [
+        [
+          "@babel/preset-env",
+          {
+            modules: false
+          }
+        ]
+      ],
+      babelrc: false
+    })
   ]
 };
